74e0086ec452d6591bdb17982e9a95e804a506fc
Federico Jalife <111152441+Fedejalife99@users.noreply.github.com>
2023-08-02 09:10:07 -0300

    Delete Airbnb backend project directory

--- Airbnb backend project/app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Reservation, Accommodation
-# Register your models here.
-
-admin.site.register(Reservation)
-admin.site.register(Accommodation)
